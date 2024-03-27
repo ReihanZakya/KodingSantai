1ef66615c70a53f64453ca4035a135a22d6e7ac5
ReihanZakya <rezakya41@gmail.com>
2024-03-27 11:55:03 +0700

    add dummy data user

--- database/seeders/DummyUsersSeeder.php
+++ database/seeders/DummyUsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DummyUsersSeeder extends Seeder
 {
@@ -12,6 +13,30 @@ class DummyUsersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $userData = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin123'),
+                'role' => 'ADMIN'
+            ],
+            [
+                'name' => 'Wawan',
+                'email' => 'wawan@gmail.com',
+                'gender' => 'male',
+                'password' => bcrypt('wawan123'),
+                'role' => 'USERS'
+            ],
+            [
+                'name' => 'Santi',
+                'email' => 'santi@gmail.com',
+                'gender' => 'female',
+                'password' => bcrypt('santi123'),
+                'role' => 'USERS'
+            ],
+        ];
+        foreach($userData as $key => $val){
+            User::create($val);
+        }
     }
 }
